bce4dca3ac259b41925c6bb63d00100b7c0651b8
bhavik <bhavik@example.com>
2026-01-09 23:19:55 +0530

    ok

--- EASY/137. Single Number II.py
+++ EASY/137. Single Number II.py
@@ -3,20 +3,19 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # h = {}
-        # for i in nums:
-        #     if not h.get(i):
-        #         h[i] = 0
-        #     h[i] += 1
-        # for i, j in h.items():
-        #     if j != 3:
-        #         return i
-        # return 0
-        i, j = 0, 0
-        for num in nums:
-            i = i ^ num & ~j
-            j = j ^ num & ~i
-        return i
+        h = {}
+        for i in nums:
+            if not h.get(i):
+                h[i] = 0
+            h[i] += 1
+            if h[i]==3:
+               del h[i]
+        return list(h)[0]
+        # i, j = 0, 0
+        # for num in nums:
+        #     i = i ^ num & ~j
+        #     j = j ^ num & ~i
+        # return i
 
 
 
