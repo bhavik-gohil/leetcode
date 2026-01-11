1e8488d46ff9f7a2de93b32a4d1b8599d4b43821
bhavik <bhavik@example.com>
2026-01-12 00:01:23 +0530

    ok!

--- 111. Minimum Depth of Binary Tree.py
+++ 111. Minimum Depth of Binary Tree.py
@@ -8,7 +8,18 @@ class TreeNode:
         self.right = right
 
 
-def insert_tree(ar, i, l):
+# def insert_tree(ar, i, l):
+#     root = None
+#     if i < l and ar[i] is not None:
+#         root = TreeNode(ar[i])
+#         root.left = insert_tree(ar, 2*i+1, l)
+#         n = 2
+#         if root.left is None:
+#             n = 1
+#         root.right = insert_tree(ar, n*i+2, l)
+#     return root
+
+def insert_tree(ar, i,  l):
     root = None
     if i < l and ar[i] is not None:
         root = TreeNode(ar[i])
@@ -19,51 +30,46 @@ def insert_tree(ar, i, l):
 
 def in_order(root):
     if root:
-        print(root.val, "::", root.left.val if root.left is not None else "-",
-              root.right.val if root.right is not None else "")
         in_order(root.left)
+        print(root.val, "::", root.left.val if root.left is not None else "N",
+              root.right.val if root.right is not None else "")
         in_order(root.right)
 
 
 class Solution:
-    def check(self, root, depth):
-        if root is None:
-            return root, root
-
-        # depth = depth + 1
-        left_depth = depth
-        right_depth = depth
-        print("..", depth, left_depth, right_depth)
-
-        if root.left is not None:
-            d = self.check(root.left, depth)
-            left_depth = left_depth + 1 + d
-
-        if root.right is not None:
-            d = self.check(root.right, depth)
-            right_depth = right_depth + 1 + d
-
-        # if left_depth == 0 and right_depth == 0:
-        #     left_depth = left_depth + 1
-        #     right_depth = right_depth + 1
-        #     print("--", depth, left_depth, right_depth)
-
-        print("-", depth, left_depth, right_depth)
-        return left_depth if left_depth > right_depth else right_depth
-
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        return self.check(root, 0)
+        def depth(root, dep):
+            if root is None:
+                return dep, dep
+
+            dep += 1
+
+            left_l, left_r = depth(root.left, dep)
+            right_l, right_r = depth(root.right, dep)
+
+            l = left_l if left_l < left_r else left_r
+            r = right_l if right_l < right_r else right_r
+
+            if left_l == left_r and left_l == dep:
+                l = r
+
+            if right_l == right_r and right_r == dep:
+                r = l
+
+            return l, r
+
+        l, r = depth(root, 0)
+
+        return l if l < r else r
 
 
 ar = [-9, -3, 2, None, 4, 4, 0, -6, None, -5]
 # ar = [3, 9, 20, None, None, 15, 7]
 # ar = [2, None, 3, None, 4, None, 5, None, 6]
 
-tree_root = insert_tree(ar, 0, len(ar))
 
-print("-------------", tree_root.val)
+tree_root = insert_tree(ar, 0, len(ar))
 in_order(tree_root)
-print("-------------")
 
 i = Solution()
 call = i.minDepth(tree_root)
---
+++ 112. Path Sum.py
@@ -0,0 +1,52 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def insert_tree(ar, i,  l):
+    root = None
+    if i < l and ar[i] is not None:
+        root = TreeNode(ar[i])
+        root.left = insert_tree(ar, 2*i+1, l)
+        root.right = insert_tree(ar, 2*i+2, l)
+    return root
+
+
+def in_order(root):
+    if root:
+        print(root.val, "::", root.left.val if root.left is not None else "N",
+              root.right.val if root.right is not None else "")
+        in_order(root.left)
+        in_order(root.right)
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def check(root, sum):
+            if root is not None:
+                sum += root.val
+                print("sum", sum, targetSum)
+                if sum == targetSum and root.left is None and root.right is None:
+                    return True
+                left = check(root.left, sum)
+                right = check(root.right, sum)
+                if left == True or right ==True:
+                    return True
+            return False
+
+        return check(root, 0)
+
+
+ar = [1]
+
+tree_root = insert_tree(ar, 0, len(ar))
+in_order(tree_root)
+
+i = Solution()
+call = i.hasPathSum(tree_root, 1)
+print(" :: ", call)
